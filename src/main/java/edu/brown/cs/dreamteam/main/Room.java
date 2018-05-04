635268bfa0b4261ee829be16c83f4a1946486407
Anina Hitt <anina@aninas-mbp.devices.brown.edu>
2018-05-04 13:53:27 -0400

    death now works fully for ais/multiplayer/humans.

--- src/main/java/edu/brown/cs/dreamteam/main/Room.java
+++ src/main/java/edu/brown/cs/dreamteam/main/Room.java
@@ -15,6 +15,7 @@ import com.google.gson.Gson;
 import edu.brown.cs.dreamteam.entity.GamePlayer;
 import edu.brown.cs.dreamteam.entity.Interactable;
 import edu.brown.cs.dreamteam.entity.Marker;
+import edu.brown.cs.dreamteam.entity.Playable;
 import edu.brown.cs.dreamteam.event.ClientState;
 import edu.brown.cs.dreamteam.event.GameEventListener;
 import edu.brown.cs.dreamteam.game.Chunk;
@@ -27,7 +28,7 @@ import networking.PlayerSession;
 
 public class Room implements GameEventListener {
   private String roomID;
-  private List<PlayerSession> players;
+  private Map<String, PlayerSession> players;
   private int threadID;
   private boolean started;
   private GameEngine engine;
@@ -39,7 +40,7 @@ public class Room implements GameEventListener {
   public Room(String roomID) {
     this.roomID = roomID;
     this.started = false;
-    players = new ArrayList<>();
+    players = new HashMap<>();
     clientStates = Maps.newConcurrentMap();
   }
 
@@ -65,7 +66,7 @@ public class Room implements GameEventListener {
 
   public void putOldClient(Session user, ClientState c) {
     if (c != null) {
-      for (PlayerSession player : players) {
+      for (PlayerSession player : players.values()) {
         if (player.getSession().equals(user)) {
           clientStates.put(player.getId(), c);
         }
@@ -74,7 +75,7 @@ public class Room implements GameEventListener {
   }
 
   public ClientState getClient(Session user) {
-    for (PlayerSession player : players) {
+    for (PlayerSession player : players.values()) {
       if (user.equals(player.getSession())) {
         return clientStates.get(player.getId());
       }
@@ -104,13 +105,13 @@ public class Room implements GameEventListener {
   }
 
   public void addPlayer(PlayerSession playerSession) {
-    players.add(playerSession);
+    players.put(playerSession.getId(), playerSession);
   }
 
   public void removePlayer(Session user) {
-    for (PlayerSession player : players) {
+    for (PlayerSession player : players.values()) {
       if (player.getSession().equals(user)) {
-        players.remove(player);
+        players.remove(player.getId());
       }
     }
   }
@@ -119,93 +120,100 @@ public class Room implements GameEventListener {
     return players.size();
   }
 
-  public List<PlayerSession> getPlayers() {
-    return players;
+  public Collection<PlayerSession> getPlayers() {
+    return players.values();
   }
 
   @Override
   public void onGameChange(ChunkMap chunks) {
-    Map<String, GamePlayer> movingThings = chunks.getPlayers();
+    Map<String, Playable> movingThings = chunks.getPlayers();
     double radius = 300.0;
     Map<String, Object> variables;
+    System.out.println("PLAYERS:");
+    System.out.println(movingThings.keySet());
     for (String id : movingThings.keySet()) {
-      GamePlayer p = movingThings.get(id);
-      if (!p.isAlive()) {
-        for (int i = 0; i < players.size(); i++) {
-          if (players.get(i).getId().equals(id)) {
-            String username = players.get(i).getUserName();
-            Session s = players.get(i).getSession();
-            s.close();
-            clientStates.remove(id);
-            players.remove(i);
-            Messenger.broadcastMessage(username + " dead.", this);
+      if (players.size() == 1 && movingThings.size() == 1) {
+        Messenger.broadcastMessage(
+            "GAME OVER: " + players.get(id).getUserName(), this);
+        return;
+      }
+      if (players.get(id) != null) {
+        GamePlayer p = (GamePlayer) movingThings.get(id);
+        System.out.println(id);
+        System.out.println(p.isAlive());
+        if (!p.isAlive() || !players.get(id).getSession().isOpen()) {
+          String username = players.get(id).getUserName();
+          Session s = players.get(id).getSession();
+          s.close();
+          clientStates.remove(id);
+          players.remove(id);
+          movingThings.get(id).kill();
+          Messenger.broadcastMessage(username + " dead.", this);
+        }
+        Collection<Chunk> chunksNeeded = chunks.chunksInRange(p, radius);
+        List<Map<String, Object>> interactables = new ArrayList<>();
+        List<Map<String, Object>> items = new ArrayList<>();
+        List<Map<String, Object>> markers = new ArrayList<>();
+        List<Map<String, Object>> inventory = new ArrayList<>();
 
-          }
+        for (Radar c : p.getInventory().getRadars()) {
+          variables = new ImmutableMap.Builder<String, Object>()
+              .put("x", c.center().x).put("y", c.center().y)
+              .put("rad", c.distance(chunks.getKeyItem())).build();
+          inventory.add(variables);
         }
 
-      }
-      Collection<Chunk> chunksNeeded = chunks.chunksInRange(p, radius);
-      List<Map<String, Object>> interactables = new ArrayList<>();
-      List<Map<String, Object>> items = new ArrayList<>();
-      List<Map<String, Object>> markers = new ArrayList<>();
-      List<Map<String, Object>> inventory = new ArrayList<>();
+        // parse out interactables
+        for (Interactable x : chunks.interactableFromChunks(chunksNeeded)) {
 
-      for (Radar c : p.getInventory().getRadars()) {
+          variables = new ImmutableMap.Builder<String, Object>()
+              .put("x", x.center().x).put("y", x.center().y)
+              .put("collisionBox", x.collisionBox()).put("hurtBox", x.hurtBox())
+              .put("drawType", x.getDrawType()).put("hitBox", x.hitBox())
+              .put("id", x.getId()).build();
+          interactables.add(variables);
+        }
+        // parse out items
+        for (Item i : ChunkMap.itemsFromChunks(chunksNeeded)) {
+          variables = new ImmutableMap.Builder<String, Object>()
+              .put("x", i.center().x).put("y", i.center().y)
+              .put("type", i.getType()).put("id", i.getId()).build();
+          items.add(variables);
+        }
+
+        // parse out player
+        Map<String, Object> z = new ImmutableMap.Builder<String, Object>()
+            .put("x", p.center().x).put("y", p.center().y).put("id", p.getId())
+            .put("type", "human").put("health", p.getHealth())
+            .put("radius", p.getRadius()).build();
+
+        for (Marker m : chunks.markers()) {
+          variables = new ImmutableMap.Builder<String, Object>()
+              .put("x", m.center().x).put("y", m.center().y)
+              .put("radius", m.getRadius()).build();
+          markers.add(variables);
+        }
+        Map<String, String> idToNames = new HashMap<>();
+        for (PlayerSession a : players.values()) {
+          idToNames.put(a.getId(), a.getUserName());
+        }
+
         variables = new ImmutableMap.Builder<String, Object>()
-            .put("x", c.center().x).put("y", c.center().y)
-            .put("rad", c.distance(chunks.getKeyItem())).build();
-        inventory.add(variables);
+            .put("type", "individual").put("player", z)
+            .put("playerList", idToNames).put("interactables", interactables)
+            .put("markers", markers).put("items", items)
+            .put("inventory", inventory)
+            .put("weapon", p.getInventory().getActiveWeapon()).build();
+        broadcastIndividualMessage(p.getId(), GSON.toJson(variables));
       }
 
-      // parse out interactables
-      for (Interactable x : chunks.interactableFromChunks(chunksNeeded)) {
-
-        variables = new ImmutableMap.Builder<String, Object>()
-            .put("x", x.center().x).put("y", x.center().y)
-            .put("collisionBox", x.collisionBox()).put("hurtBox", x.hurtBox())
-            .put("drawType", x.getDrawType()).put("hitBox", x.hitBox())
-            .put("id", x.getId()).build();
-        interactables.add(variables);
-      }
-      // parse out items
-      for (Item i : ChunkMap.itemsFromChunks(chunksNeeded)) {
-        variables = new ImmutableMap.Builder<String, Object>()
-            .put("x", i.center().x).put("y", i.center().y)
-            .put("type", i.getType()).put("id", i.getId()).build();
-        items.add(variables);
-      }
-
-      // parse out player
-      Map<String, Object> z = new ImmutableMap.Builder<String, Object>()
-          .put("x", p.center().x).put("y", p.center().y).put("id", p.getId())
-          .put("type", "human").put("health", p.getHealth())
-          .put("radius", p.getRadius()).build();
-
-      for (Marker m : chunks.markers()) {
-        variables = new ImmutableMap.Builder<String, Object>()
-            .put("x", m.center().x).put("y", m.center().y)
-            .put("radius", m.getRadius()).build();
-        markers.add(variables);
-      }
-      Map<String, String> idToNames = new HashMap<>();
-      for (PlayerSession a : players) {
-        idToNames.put(a.getId(), a.getUserName());
-      }
-
-      variables = new ImmutableMap.Builder<String, Object>()
-          .put("type", "individual").put("player", z)
-          .put("playerList", idToNames).put("interactables", interactables)
-          .put("markers", markers).put("items", items)
-          .put("inventory", inventory)
-          .put("weapon", p.getInventory().getActiveWeapon()).build();
-      broadcastIndividualMessage(p.getId(), GSON.toJson(variables));
     }
 
   }
 
   public void broadcastIndividualMessage(String sender, String message) {
     Session relevant = null;
-    for (PlayerSession player : players) {
+    for (PlayerSession player : players.values()) {
       if (player.getId().equals(sender)) {
         relevant = player.getSession();
       }
@@ -215,6 +223,8 @@ public class Room implements GameEventListener {
       try {
         relevant.getRemote().sendString(message);
       } catch (Exception e) {
+        System.out.println(
+            "somehow after all these checks you somehow managed to try to send a message to a session that was null or not open. well done.");
         e.printStackTrace();
       }
 
